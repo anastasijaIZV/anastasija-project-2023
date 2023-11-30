0ecad2346b2a4de10ec7e3614c055f7bd9e4e18f
anastasijaIZV <148444839+anastasijaIZV@users.noreply.github.com>
2023-11-30 07:39:25 +0200

    working (10 customers made) 30.11

--- src/main/java/com/example/demo1/Project2023Demo1Application.java
+++ src/main/java/com/example/demo1/Project2023Demo1Application.java
@@ -1,8 +1,12 @@
 package com.example.demo1;
 
+import com.lp.domain.model.Customer;
+import com.lp.domain.service.CustomerService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -12,13 +16,27 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @ComponentScan(basePackages = {
         "com.lp.domain"
 })
+public class Project2023Demo1Application implements CommandLineRunner {
+    private final CustomerService customerService;
 
-public class Project2023Demo1Application {
+    public Project2023Demo1Application(CustomerService customerService) {
+        this.customerService = customerService;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Project2023Demo1Application.class, args);
     }
+
     public void run(String... args) {
+            for (int i = 0; i < 10; i++) {
+                Customer c = new Customer();
+                c.setName("Customer " + i);
+                c.setAffiliate(
+                        customerService.getOwner()
+                );
+                customerService.saveCustomer(c);
+            }
+            customerService.getAllCustomers().forEach(System.out::println);
 
     }
 }
